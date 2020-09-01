1f514be7ebc96b6d7341077b236ea33ba7e20edd
fengbo <342077837@qq.com>
2020-09-01 18:48:43 +0800

    再增加一点文件

---
+++ KSDanmuViewDemo/Danmu/shortVideo/Player/AVPlayerManager.h
@@ -0,0 +1,23 @@
+//
+//  AVPlayerManager.h
+//  KSDanmuViewDemo
+//
+//  Created by YaphetS on 2020/9/1.
+//  Copyright © 2020 sapphire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface AVPlayerManager:NSObject
+@property (nonatomic, strong) NSMutableArray<AVPlayer *>   *playerArray;  //用于存储AVPlayer的数组
+
++ (AVPlayerManager *)shareManager;
++ (void)setAudioMode;
+- (void)play:(AVPlayer *)player;
+- (void)pause:(AVPlayer *)player;
+- (void)pauseAll;
+- (void)replay:(AVPlayer *)player;
+- (void)removeAllPlayers;
+
+@end
